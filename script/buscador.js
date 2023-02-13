b477035401a8251c0c3f2a55c47d1e171ea599e5
LourdesAlsina <lulialsinaa@gmail.com>
2023-02-12 21:12:38 -0300

    correccion

--- script/buscador.js
+++ script/buscador.js
@@ -1,14 +1,14 @@
 window.onload = function(){
 
 const products = [
-    {id:1, producto: "Iphone 13 Pro Max Blue", tipo: "Celular", modelo: "13 Pro Max ", precio: 150000, anio: 2022, stock: 7, imagen:"front (1).png", detalleP: "productDetail1.html" },
-    {id:2, producto:"Iphone 13 Pro Max Gold", tipo: "Celular", modelo: "13 Pro Max gold", precio: 150000, anio: 2022, stock: 3, imagen:"front.png",detalleP: "productDetail2.html"},
-    {id:3, producto:"Iphone 13 Max gold Green", tipo: "Celular",modelo: "13 Max gold green", precio: 150000, anio: 2022, stock: 5, imagen:"iphone verde.jpg", detalleP: "productDetail3.html"},
-    {id:4, producto:"Iphone 12 Black", tipo: "Celular", modelo: "12 black", precio: 100000, anio: 2020, stock: 10, imagen:"ihpone black.jpg", detalleP: "productDetail4.html"},
-    {id:5, producto:"Funda blanca", tipo: "Funda", modelo:"12", precio: 150000, stock: 8, imagen:"MWVX2ZM-A.jpg", detalleP: "productDetail6.html"},
-    {id:6, producto:"Funda aqua", tipo: "Funda", modelo:"12",  precio: 150000, stock: 5, imagen:"MY182.jpg", detalleP: "productDetail7.html"},
-    {id:7, producto:"Funda negra", tipo: "Funda", modelo:"12",  precio: 3000, stock: 9, imagen:"funda negra.webp", detalleP: "productDetail8.html"},
-    {id:8, producto:"Funda rosa", tipo: "Funda", modelo:"13",  precio: 3000, stock: 1, imagen:"funda-silicona-para-iphone-13-con-magsafe-chalk-pink.jpg", detalleP: "productDetail9.html"},
+    {id:1, producto: "Iphone 13 Pro Max Blue", clase:"phone1", tipo: "Celular", modelo: "13 Pro Max ", precio: 150000, anio: 2022, stock: 7, imagen:"front (1).png", detalleP: "productDetail1.html" },
+    {id:2, producto:"Iphone 13 Pro Max Gold", clase:"phone2", tipo: "Celular", modelo: "13 Pro Max gold", precio: 150000, anio: 2022, stock: 3, imagen:"front.png",detalleP: "productDetail2.html"},
+    {id:3, producto:"Iphone 13 Max gold Green", clase:"phone3", tipo: "Celular",modelo: "13 Max gold green", precio: 150000, anio: 2022, stock: 5, imagen:"iphone verde.jpg", detalleP: "productDetail3.html"},
+    {id:4, producto:"Iphone 12 Black", clase:"phone4", tipo: "Celular", modelo: "12 black", precio: 100000, anio: 2020, stock: 10, imagen:"ihpone black.jpg", detalleP: "productDetail4.html"},
+    {id:5, producto:"Funda blanca", clase:"funda1", tipo: "Funda", modelo:"12", precio: 150000, stock: 8, imagen:"MWVX2ZM-A.jpg", detalleP: "productDetail6.html"},
+    {id:6, producto:"Funda aqua", clase:"funda2", tipo: "Funda", modelo:"12",  precio: 150000, stock: 5, imagen:"MY182.jpg", detalleP: "productDetail7.html"},
+    {id:7, producto:"Funda negra", clase:"funda3", tipo: "Funda", modelo:"12",  precio: 3000, stock: 9, imagen:"funda negra.webp", detalleP: "productDetail8.html"},
+    {id:8, producto:"Funda rosa", clase:"funda4", tipo: "Funda", modelo:"13",  precio: 3000, stock: 1, imagen:"funda-silicona-para-iphone-13-con-magsafe-chalk-pink.jpg", detalleP: "productDetail9.html"},
 ]
 
 /*let contenedorProducts = document.querySelector(".contenedorPoroducts")
@@ -69,7 +69,7 @@ function renderProds(arrayCualquiera) {
     for (let i=0; i < arrayCualquiera.length; i++) {
         if(arrayCualquiera[i].tipo == "Celular"){
             contenedorCelulares.innerHTML += `            
-        <div id="phone${i+1}" class="product">
+        <div id="${arrayCualquiera[i].clase}" class="product">
             <div class="prodImg">
                 <a href="../productDetails/${arrayCualquiera[i].detalleP}"><img src="../img/${arrayCualquiera[i].imagen}" alt="${arrayCualquiera[i].producto}"></a> 
             </div>
@@ -85,7 +85,7 @@ function renderProds(arrayCualquiera) {
         }
         else if(arrayCualquiera[i].tipo == "Funda"){
             contenedorAccesorios.innerHTML += `                       
-                <div id="funda${i-3}" class="product">
+                <div id="${arrayCualquiera[i].clase}" class="product">
                     <div class="prodImg">
                         <a href="../productDetails/${arrayCualquiera[i].detalleP}"> <img src="../img/${arrayCualquiera[i].imagen}" alt="${arrayCualquiera[i].producto}"></a>
                     </div>                
